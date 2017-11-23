522d12e0f72e2850a8e48adfd0de6e0fbb54c21c
Enova <17408285+Enovale@users.noreply.github.com>
2017-11-23 09:26:44 -0600

    Update deepfry.py

--- deepfry/deepfry.py
+++ deepfry/deepfry.py
@@ -28,6 +28,8 @@ class DeepFry:
     async def deepfry(self, ctx, link):
         """Will deep fry any image link given. (Adds absurd saturation, etc)"""
 
+        if any(word in ".BMP .EPS .GIF .ICNS .IM .JPEG .MSP .PCX .PNG .PPM .SPIDER .TIFF .WebP .XBM .CUR .DCX .DDS .FLI .FLC .FPX .FTEX .GBR .GD .ICO .IMT .IPTC .NAA .MCIDAS .MIC .MPO .PCD .PIXAR .PSD .SGI .TGA .WAL .XPM .PALM .PDF .BUFR .FITS .GRIB .HDF5 .MPEG .WMF .bmp .eps .gif .icns .im .jpeg .msp .pcx .png .ppm .spider .tiff .webp .xbm .cur .dcx .dds .fli .flc .fpx .ftex .gbr .gd .ico .imt .iptc .naa .mcidas .mic .mpo .pcd .pixar .psd .sgi .tga .wal .xpm .palm .pdf .bufr .fits .grib .hdf5 .mpeg .wmf" for word in link):
+        
         id = ctx.message.author.id
         channel = ctx.message.channel
         response = requests.get(link)
