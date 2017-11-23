a7e9a027f25ff139501a512ac3dd5e6d1ae8270b
Enova <17408285+Enovale@users.noreply.github.com>
2017-11-23 09:31:15 -0600

    Update deepfry.py

--- deepfry/deepfry.py
+++ deepfry/deepfry.py
@@ -30,25 +30,27 @@ class DeepFry:
 
         if any(word in ".BMP .EPS .GIF .ICNS .IM .JPEG .MSP .PCX .PNG .PPM .SPIDER .TIFF .WebP .XBM .CUR .DCX .DDS .FLI .FLC .FPX .FTEX .GBR .GD .ICO .IMT .IPTC .NAA .MCIDAS .MIC .MPO .PCD .PIXAR .PSD .SGI .TGA .WAL .XPM .PALM .PDF .BUFR .FITS .GRIB .HDF5 .MPEG .WMF .bmp .eps .gif .icns .im .jpeg .msp .pcx .png .ppm .spider .tiff .webp .xbm .cur .dcx .dds .fli .flc .fpx .ftex .gbr .gd .ico .imt .iptc .naa .mcidas .mic .mpo .pcd .pixar .psd .sgi .tga .wal .xpm .palm .pdf .bufr .fits .grib .hdf5 .mpeg .wmf" for word in link):
         
-        id = ctx.message.author.id
-        channel = ctx.message.channel
-        response = requests.get(link)
-        image = Image.open (BytesIO(response.content))
+            id = ctx.message.author.id
+            channel = ctx.message.channel
+            response = requests.get(link)
+            image = Image.open (BytesIO(response.content))
         
 
-        contrast = ImageEnhance.Contrast( image )
-        brightness = ImageEnhance.Brightness( image )
-        sharpness = ImageEnhance.Sharpness( image )
-        saturation = ImageEnhance.Color( image )
-        image = sharpness.enhance(5.0)
-        image = brightness.enhance( 1.5 )
-        image = contrast.enhance( 5.0 )
-        image = saturation.enhance( 7.0 )
-        image = image.filter(ImageFilter.GaussianBlur(1))
-        image = image.convert("RGB")
-        image.save(self.path + "/" + id + ".jpg", quality=8)
-        await self.bot.send_file(channel, self.path + "/" + id + ".jpg")
-        os.remove(self.path + "/" + id + ".jpg")
+       	    contrast = ImageEnhance.Contrast( image )
+       	    brightness = ImageEnhance.Brightness( image )
+       	    sharpness = ImageEnhance.Sharpness( image )
+       	    saturation = ImageEnhance.Color( image )
+       	    image = sharpness.enhance(5.0)
+       	    image = brightness.enhance( 1.5 )
+       	    image = contrast.enhance( 5.0 )
+       	    image = saturation.enhance( 7.0 )
+       	    image = image.filter(ImageFilter.GaussianBlur(1))
+       	    image = image.convert("RGB")
+            image.save(self.path + "/" + id + ".jpg", quality=8)
+       		await self.bot.send_file(channel, self.path + "/" + id + ".jpg")
+       		os.remove(self.path + "/" + id + ".jpg")
+		else:
+			await self.bot.say("Sorry, but this image format is not supported.")
         
     @commands.command(pass_context=True)
     async def customfry(self, ctx, link, contrast, saturation, sharpness, brightness, blur):
