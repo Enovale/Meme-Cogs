0428f1d8ed8c6f98963ae5cf8c9ff59aff28396e
Enova <17408285+Enovale@users.noreply.github.com>
2017-11-28 12:09:20 -0600

    Update shoot.py

--- shoot/shoot.py
+++ shoot/shoot.py
@@ -13,8 +13,10 @@ class Shoot:
         
         #Your code will go here
         if '@everyone' in message.content:
-            if 'school' in school:
-                await self.bot.say(ctx.message.author.mention + "shot up the local school, killing hundreds.")
+            if school == 'school':
+                await self.bot.say(ctx.message.author.mention + " shot up the local school, killing hundreds.")
+            else if school == 'server':
+                await self.bot.say(ctx.message.author.mention + " shot up the server.")
         if ctx.message.author.id != user.id:
             await self.bot.say(ctx.message.author.mention + " gunned down " + user.mention + " !")
         else if ctx.message.author.id == user.id:
