53fd336f2d6a8b33a001f29eaf8d9876ee279cf7
Enova <17408285+Enovale@users.noreply.github.com>
2017-11-30 17:37:08 -0600

    Create filter.py

---
+++ filter/filter.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+import os
+import asyncio
+from cogs.utils.dataIO import dataIO
+import PIL
+from PIL import Image
+import glob, os
+from PIL import ImageEnhance
+from PIL import ImageColor
+from PIL import ImageFilter
+import requests
+from io import BytesIO
+
+WINDOWS_OS = os.name == 'nt'
+
+class filter:
+    """Allows for various image filtering"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        if not os.path.exists("data/deepfry"):
+            os.makedirs("data/deepfry")
+        self.path = os.path.join("data", "deepfry")
+
+        
+    @commands.command(pass_context=True)
+    async def invert(self, ctx, link):
+        """Inverts the given image"""
+
+        if any(word in ".BMP .EPS .GIF .ICNS .IM .JPEG .MSP .PCX .PNG .PPM .SPIDER .TIFF .WebP .XBM .CUR .DCX .DDS .FLI .FLC .FPX .FTEX .GBR .GD .ICO .IMT .IPTC .NAA .MCIDAS .MIC .MPO .PCD .PIXAR .PSD .SGI .TGA .WAL .XPM .PALM .PDF .BUFR .FITS .GRIB .HDF5 .MPEG .WMF .bmp .eps .gif .icns .im .jpeg .msp .pcx .png .ppm .spider .tiff .webp .xbm .cur .dcx .dds .fli .flc .fpx .ftex .gbr .gd .ico .imt .iptc .naa .mcidas .mic .mpo .pcd .pixar .psd .sgi .tga .wal .xpm .palm .pdf .bufr .fits .grib .hdf5 .mpeg .wmf" for word in link):
+        
+            id = ctx.message.author.id
+            channel = ctx.message.channel
+            response = requests.get(link)
+            image = Image.open (BytesIO(response.content))
+        
+
+       	    image = ImageOps.invert(image)
+       	    image = image.convert("RGB")
+            image.save(self.path + "/" + id + ".jpg", quality=8)
+       		await self.bot.send_file(channel, self.path + "/" + id + ".jpg")
+       		os.remove(self.path + "/" + id + ".jpg")
+		else:
+			await self.bot.say("Sorry, but this image format is not supported.")
+
+def setup(bot):
+    bot.add_cog(filter(bot))
