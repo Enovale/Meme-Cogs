b36bebd731ba801484257f4adb47e0eb97acad7e
Enova <17408285+Enovale@users.noreply.github.com>
2017-12-01 15:47:39 -0600

    Update meme.py

--- meme/meme.py
+++ meme/meme.py
@@ -1,25 +1,12 @@
 import asyncio, aiohttp, discord
-#import aalib
 import os, sys, linecache, traceback, glob
 import re, json, random, math, html
-#import wand, wand.color, wand.drawing
 import PIL, PIL.Image, PIL.ImageFont, PIL.ImageOps, PIL.ImageDraw
-import numpy as np
-import cairosvg, jpglitch, urbandict
-#import pixelsort.sorter, pixelsort.sorting, pixelsort.util, pixelsort.interval
 import hashlib, base64
 from vw import macintoshplus
-from urllib.parse import parse_qs
-from lxml import etree
 from imgurpython import ImgurClient
 from io import BytesIO, StringIO
 from discord.ext import commands
-from utils import checks
-from pyfiglet import figlet_format
-from string import ascii_lowercase as alphabet
-from urllib.parse import quote
-from mods.cog import Cog
-from concurrent.futures._base import CancelledError
 
 class meme:
     """Various Meme Retrieving/Making commands"""
