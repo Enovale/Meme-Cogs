a637420e64b630cfa091733be82a211fe194d5cd
Enova <17408285+Enovale@users.noreply.github.com>
2017-11-30 18:48:11 -0600

    Update imagefilter.py

--- imagefilter/imagefilter.py
+++ imagefilter/imagefilter.py
@@ -45,7 +45,7 @@ class imagefilter:
             ##await self.bot.say("Sorry, but this image format is not supported.")
             
     @commands.command(pass_context=True)
-    async def crop(self, ctx, pixels):
+    async def crop(self, ctx, link, pixels):
         """crops the given image by however many pixels you specify."""
 
         if any(word in ".BMP .EPS .GIF .ICNS .IM .JPEG .MSP .PCX .PNG .PPM .SPIDER .TIFF .WebP .XBM .CUR .DCX .DDS .FLI .FLC .FPX .FTEX .GBR .GD .ICO .IMT .IPTC .NAA .MCIDAS .MIC .MPO .PCD .PIXAR .PSD .SGI .TGA .WAL .XPM .PALM .PDF .BUFR .FITS .GRIB .HDF5 .MPEG .WMF .bmp .eps .gif .icns .im .jpeg .msp .pcx .png .ppm .spider .tiff .webp .xbm .cur .dcx .dds .fli .flc .fpx .ftex .gbr .gd .ico .imt .iptc .naa .mcidas .mic .mpo .pcd .pixar .psd .sgi .tga .wal .xpm .palm .pdf .bufr .fits .grib .hdf5 .mpeg .wmf" for word in link):
