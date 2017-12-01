a91daeeeb77381b6cea71be08a5d0a1bc76924fa
Enova <17408285+Enovale@users.noreply.github.com>
2017-11-30 18:47:43 -0600

    Update imagefilter.py

--- imagefilter/imagefilter.py
+++ imagefilter/imagefilter.py
@@ -43,6 +43,23 @@ class imagefilter:
             os.remove(self.path + "/" + id + ".jpg")
         ##else:
             ##await self.bot.say("Sorry, but this image format is not supported.")
+            
+    @commands.command(pass_context=True)
+    async def crop(self, ctx, pixels):
+        """crops the given image by however many pixels you specify."""
+
+        if any(word in ".BMP .EPS .GIF .ICNS .IM .JPEG .MSP .PCX .PNG .PPM .SPIDER .TIFF .WebP .XBM .CUR .DCX .DDS .FLI .FLC .FPX .FTEX .GBR .GD .ICO .IMT .IPTC .NAA .MCIDAS .MIC .MPO .PCD .PIXAR .PSD .SGI .TGA .WAL .XPM .PALM .PDF .BUFR .FITS .GRIB .HDF5 .MPEG .WMF .bmp .eps .gif .icns .im .jpeg .msp .pcx .png .ppm .spider .tiff .webp .xbm .cur .dcx .dds .fli .flc .fpx .ftex .gbr .gd .ico .imt .iptc .naa .mcidas .mic .mpo .pcd .pixar .psd .sgi .tga .wal .xpm .palm .pdf .bufr .fits .grib .hdf5 .mpeg .wmf" for word in link):
+        
+            id = ctx.message.author.id
+            channel = ctx.message.channel
+            response = requests.get(link)
+            image = Image.open (BytesIO(response.content))
+        
+            image = image.convert("RGB")
+       	    image = ImageOps.crop(image, pixels)
+            image.save(self.path + "/" + id + ".jpg", quality=8)
+       	    await self.bot.send_file(channel, self.path + "/" + id + ".jpg")
+            os.remove(self.path + "/" + id + ".jpg")
 
 def setup(bot):
     bot.add_cog(imagefilter(bot))
