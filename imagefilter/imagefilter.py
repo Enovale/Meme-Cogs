e46ac0e8756de771b1c6ff3fa8425b2ab7a8f165
Enova <17408285+Enovale@users.noreply.github.com>
2017-12-02 13:27:42 -0600

    Update imagefilter.py

--- imagefilter/imagefilter.py
+++ imagefilter/imagefilter.py
@@ -103,7 +103,8 @@ class imagefilter:
     @commands.cooldown(1, 5)
     async def ascii(self, ctx, *, text:str):
         """Convert text into ASCII"""
-        await self.bot.say("```" + figlet_format(ctx.message.content, font='starwars') + "```")
+        asciitext = figlet_format(ctx.message.content, font='starwars')
+        await self.bot.say("```" + asciitext + "```")
 
 def setup(bot):
     bot.add_cog(imagefilter(bot))
