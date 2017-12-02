d96a846e87dd4a6045cadb5a25bd9a4728bde515
Enova <17408285+Enovale@users.noreply.github.com>
2017-12-02 13:19:19 -0600

    Update imagefilter.py

--- imagefilter/imagefilter.py
+++ imagefilter/imagefilter.py
@@ -12,6 +12,11 @@ from PIL import ImageFilter
 from PIL import ImageOps
 import requests
 from io import BytesIO
+import sys
+from colorama import init
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+from termcolor import cprint 
+from pyfiglet import figlet_format
 
 WINDOWS_OS = os.name == 'nt'
 
@@ -98,38 +103,8 @@ class imagefilter:
     @commands.cooldown(1, 5)
     async def ascii(self, ctx, *, text:str):
         """Convert text into ASCII"""
-        if len(ctx.message.content) > 1000:
-            await self.bot.say("2 long asshole")
-            return
-        if ctx.message.content == 'donger' or ctx.message.content == 'dong':
-            ctx.message.content = "8====D"
-        if len(ctx.message.content) >= 1999:
-            await self.gist(ctx, ctx.message.content, ctx.message.content)
-            msg = None
-        elif len(ctx.message.content) <= 600:
-            msg = "```fix\n{0}```".format(ctx.message.content)
-        else:
-            msg = None
-            await self.bot.send_file(ctx.message.channel, filename='ascii.png', content=msg)
-
-    def generate_ascii(self, image):
-        font = PIL.ImageFont.truetype(self.files_path('FreeMonoBold.ttf'), 15, encoding="unic")
-        image_width, image_height = image.size
-        aalib_screen_width= int(image_width/24.9)*10
-        aalib_screen_height= int(image_height/41.39)*10
-        screen = aalib.AsciiScreen(width=aalib_screen_width, height=aalib_screen_height)
-        im = image.convert("L").resize(screen.virtual_size)
-        screen.put_image((0,0), im)
-        y = 0
-        how_many_rows = len(screen.render().splitlines()) 
-        new_img_width, font_size = font.getsize(screen.render().splitlines()[0])
-        img = PIL.Image.new("RGBA", (new_img_width, how_many_rows*15), (255,255,255))
-        draw = PIL.ImageDraw.Draw(img)
-        for lines in screen.render().splitlines():
-            draw.text((0,y), lines, (0,0,0), font=font)
-            y = y + 15
-        imagefit = PIL.ImageOps.fit(img, (image_width, image_height), PIL.Image.ANTIALIAS)
-        return imagefit
+        cprint(figlet_format('missile!', font='starwars'),
+        'yellow', 'on_red', attrs=['bold'])
 
 def setup(bot):
     bot.add_cog(imagefilter(bot))
