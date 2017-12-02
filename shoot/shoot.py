726c0b7b8963817d7bf23e2978aacba7c835f01b
Enova <17408285+Enovale@users.noreply.github.com>
2017-12-01 20:16:30 -0600

    Update shoot.py

--- shoot/shoot.py
+++ shoot/shoot.py
@@ -8,18 +8,18 @@ class Shoot:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def shoot(self, ctx, user : discord.Member = None):
+    async def shoot(self, ctx, user : discord.Member = None, everyone = None):
         """Will shoot the user specified, (be careful with that gun!)"""
         
         #Your code will go here
-        ##if not user:
-            ##if 'everyone' in ctx.message.content:
+        if not user:
+            if everyone == 'everyone':
                 ##if school == 'school':
                 ##    await self.bot.say(ctx.message.author.mention + " shot up the local school, killing hundreds.")
                 ##else if school == 'server':
                 ##    await self.bot.say(ctx.message.author.mention + " shot up the server.")
                 ##else:
-                ##await self.bot.say(ctx.message.author.mention + " shot everyone in existence".)
+                await self.bot.say(ctx.message.author.mention + " shot everyone in existence".)
         if ctx.message.author.id != user.id:
             await self.bot.say(ctx.message.author.mention + " gunned down " + user.mention + " !")
         if ctx.message.author.id == user.id:
