2b8d78cf74d4c52196d8b1247566f60d08c026dd
Enova <17408285+Enovale@users.noreply.github.com>
2017-12-04 16:55:56 -0600

    Update imagefilter.py

--- imagefilter/imagefilter.py
+++ imagefilter/imagefilter.py
@@ -193,6 +193,8 @@ class imagefilter:
             # draw.text((x, y),"Sample Text",(r,g,b))
             draw.multiline_text((50, 20),"Uh oh! You friccin\nmoron. You just got",(0,0,0),font=font, align='center')
             draw.multiline_text((50, 700),"Tag your friends to\ntotally BEAN! them!",(0,0,0),font=font, align='center')
+            mask = image.convert("L").point(lambda x: min(x, 100))
+            image.putalpha(mask)
             image.save('sample-out.jpg')
             bean_path = 'bean.png'
             bean = PIL.Image.open(bean_path)
