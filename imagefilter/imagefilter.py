c2b3113c65b3ffb386ab6f335bcf4d5d3b1f8b82
Enova <17408285+Enovale@users.noreply.github.com>
2017-12-04 11:24:33 -0600

    Update imagefilter.py

--- imagefilter/imagefilter.py
+++ imagefilter/imagefilter.py
@@ -190,7 +190,7 @@ class imagefilter:
             bean_path = 'bean.png'
             bean = PIL.Image.open(bean_path)
             response = requests.get(user)
-            img = Image.open (BytesIO(user))
+            img = Image.open (user)
             width, height = bean.size
             width2, height2 = img.size
             img = img.resize((334, 395))
