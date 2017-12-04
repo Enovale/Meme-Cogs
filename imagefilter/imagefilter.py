0ff7d47488c2f2093b7768cd73275e275f9d2b9a
Enova <17408285+Enovale@users.noreply.github.com>
2017-12-04 15:42:06 -0600

    Update imagefilter.py

--- imagefilter/imagefilter.py
+++ imagefilter/imagefilter.py
@@ -191,7 +191,7 @@ class imagefilter:
 
             im = Image.new("RGBA",(W,H),"white")
             draw = ImageDraw.Draw(im)
-            fnt = ImageFont.truetype('Arial Narrow.ttf')
+            fnt = ImageFont.truetype('arialn.ttf', 80)
             w, h = draw.textsize(msg, font=fnt)
             draw.text(((W-w)/2,(H-h)), msg, fill="black")
 
