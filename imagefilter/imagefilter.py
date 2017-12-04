8d3beef2505de626347dd6a9e3b48382349d62a7
Enova <17408285+Enovale@users.noreply.github.com>
2017-12-04 15:12:00 -0600

    Update imagefilter.py

--- imagefilter/imagefilter.py
+++ imagefilter/imagefilter.py
@@ -191,8 +191,8 @@ class imagefilter:
 
             im = Image.new("RGBA",(W,H),"white")
             draw = ImageDraw.Draw(im)
-            fnt = ImageFont.truetype('Verdana.ttf', 120)
-            w, h = draw.textsize(msg, font='Verdana.ttf')
+            fnt = ImageFont.load('Verdana.ttf', 120)
+            w, h = draw.textsize(msg, font=fnt)
             draw.text(((W-w)/2,(H-h)), msg, fill="black")
 
             im.save("hello.png", "PNG")
