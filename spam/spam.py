bbe4f5b31f13558f488d0287253ec04e1e117fd8
Enova <17408285+Enovale@users.noreply.github.com>
2018-01-07 14:15:42 -0600

    Update spam.py

--- spam/spam.py
+++ spam/spam.py
@@ -10,6 +10,12 @@ class Spam:
 	async def spam(self, ctx, user=None, number: int=None, spam_text: str=None):
 		"""Spam The User hehe"""
 		
+		elif len(ctx.message.mentions):
+			user = ctx.message.mentions[0]
+		if type(user) == discord.User or type(user) == discord.Member:
+			notneeded = True
+		elif user == "@everyone":
+			user = "@everyone"
 		if spam_text == None:
 			await self.bot.say('Wait What Dude Want To Spam Sombody Nothing Wew')
 			return
