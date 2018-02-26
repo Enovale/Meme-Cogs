2eb821f9a02c47ce601351b369194ee233a94066
Enova <17408285+Enovale@users.noreply.github.com>
2018-02-26 07:20:12 -0600

    Create help.py

---
+++ help/help.py
@@ -0,0 +1,24 @@
+import asyncio
+from discord.ext import commands
+import discord
+import inspect
+import sys
+import traceback
+import re
+
+class blankhelp:
+    """Custimized Help For LBB"""
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(pass_context=True, hidden=True)
+    async def help(self, ctx):
+        """Shows This Command"""
+        destination = author if self.bot.pm_help else ctx.message.channel
+        await self.bot.send_message(destination, "Thank's for using Luigi's Balloon Bot! Simply use ```b!findit or b!hideit``` to play!")
+
+def setup(bot):
+    bot.remove_command('help')
+    n = blankhelp(bot)
+    bot.add_cog(n)
